d8ed9410bf1d6b60b4ab237c2dfe064638f7744f
3nDr0n <3nDr0n@3NDR0N-PC>
2023-07-04 10:25:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, what is your name?");  //displays question
+            string name = Console.ReadLine();  //creates string variable with value given by user
+            Console.WriteLine("How is your day? Please answer with one word.");  //displays question
+            string day = Console.ReadLine();  //creates string variable with value given by user
+            string dayUpper = day.ToUpper();   //variable that converts another variable to uppercase
+            Console.WriteLine("So " + name + ", your day is " + dayUpper + ", why is that?");  //concatenated strings
+            Console.ReadLine();  //any user input
+            StringBuilder sb = new StringBuilder();  //Paragraph built line by line using stringbuilder
+            sb.AppendLine(name + " I hope you know that your are awesome and worthy of love.");
+            sb.AppendLine("Sometimes we forget that we are unique and we do not need to compare ourselves to others.");
+            sb.AppendLine("Learn to love yourself because you are the only one who will always be there for you.");
+            Console.WriteLine(sb);  //Displays paragraph built using stringbuilder
+            Console.ReadLine() ;    
+        }
+    }
+}
